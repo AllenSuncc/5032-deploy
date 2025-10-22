11b09539acd2ab36be51977ca5d3c0c2fb1e4bd8
Haojun Sun <hsun0066@student.monash.edu>
2025-10-23 00:35:25 +1100

    build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/5032-deploy/',   // 👈 必加这一行，和你的仓库名一致！
   plugins: [
     vue(),
     vueDevTools(),
